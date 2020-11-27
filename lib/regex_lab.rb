4b9d1c626d35e4f3787971d66acaab29a18ed21f
Nsingletary <singletarybush@gmail.com>
2020-11-27 11:54:00 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,7 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-
+    return text.scan(/\b[a-zA-Z])
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
@@ -19,5 +19,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+    phone.match(/\D*\d{3}\D*\d{3}\D*\d{4}/)
 end
