4dc2e130602c8ddd9bbe65456e7c1db9cb2a4872
Nsingletary <singletarybush@gmail.com>
2020-11-27 11:55:53 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    if text.match(/\A[A-Z].+[.!]\z/)
+        return true	    return true
+    else	  else
+        return false	    return false
+    end
 end
 
 def valid_phone_number?(phone)
